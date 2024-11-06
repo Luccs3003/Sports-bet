bcd8f8b95b0b44862f67f1b59f7656e2e188f92a
Maickon_Brenner <maickonbrenner@gmail.com>
2024-11-06 18:14:44 -0300

    Atualizando menu inicial

--- sports-bet/src/sports_bet/core.clj
+++ sports-bet/src/sports_bet/core.clj
@@ -5,20 +5,23 @@
 
 (defn listar-esportes []
   (def esportes (api/obter-esportes))
-    (println "Esportes disponíveis:")
     (dorun (map #(println (:name %)) (vals esportes))))
 
 (defn menu []
   (println "\n=== MENU DE APOSTAS ESPORTIVAS ===")
   (println "1. Listar Esportes")
   (println "2. Gerenciar Conta")
-  (println "3. Sair")
-  (print "Escolha uma opção:\n ")
+  (println "3. Fazer Aposta")
+  (println "4. Consultar Resultados")
+  (println "5. Sair")
+  (println "Escolha uma opção: ")
   (def opcao (read-line))
     (case opcao
-      "1" (do (listar-esportes) (menu))
+      "1" (do (println "Esportes disponíveis:") (listar-esportes) (menu))
       "2" (do (conta/menu-conta) (menu))
-      "3" (println "Saindo...")
+      "3" (do () (menu))
+      "4" (do () (menu))
+      "5" (println "Saindo...")
       (do (println "Opção inválida!") (menu))))
 
 (defn -main [& args]
