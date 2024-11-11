e49428bd7f84992b0a3faf00e6334cf3f6b4000f
Lucas Cardoso <lucascardoso4283@gmail.com>
2024-11-11 18:57:11 -0300

    Update api.clj

--- sports-bet/src/sports_bet/api.clj
+++ sports-bet/src/sports_bet/api.clj
@@ -7,9 +7,7 @@
 
 (defn obter-esportes []
   (let [url "https://betano.p.rapidapi.com/sports"
-        headers {:headers {:x-rapidapi-key "26a0903448mshb6ee91b73f997a7p1362d5jsnbd976f17f5f4"
-                           :x-rapidapi-host "betano.p.rapidapi.com"}}
-        response (client/get url headers)]
+        response (client/get url {:headers api-headers})]
     (if (= 200 (:status response))
       (json/parse-string (:body response) true)
       (println "Erro ao obter esportes:" (:status response)))))
