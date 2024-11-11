bce385dd4f80ad4ad41a36afe5fcf66fa781f24e
Lucas Cardoso <lucascardoso4283@gmail.com>
2024-11-11 18:34:12 -0300

    Update api.clj

--- sports-bet/src/sports_bet/api.clj
+++ sports-bet/src/sports_bet/api.clj
@@ -2,6 +2,9 @@
   (:require [clj-http.client :as client]
             [cheshire.core :as json]))
 
+(def api-headers {:x-rapidapi-key "26a0903448mshb6ee91b73f997a7p1362d5jsnbd976f17f5f4"
+                  :x-rapidapi-host "betano.p.rapidapi.com"})
+
 (defn obter-esportes []
   (let [url "https://betano.p.rapidapi.com/sports"
         headers {:headers {:x-rapidapi-key "26a0903448mshb6ee91b73f997a7p1362d5jsnbd976f17f5f4"
@@ -10,3 +13,33 @@
     (if (= 200 (:status response))
       (json/parse-string (:body response) true)
       (println "Erro ao obter esportes:" (:status response)))))
+
+(defn obter-oddstypes [sport]
+  (let [url "https://betano.p.rapidapi.com/oddstypes"
+        response (client/get url {:headers api-headers :query-params {:sport sport}})]
+    (if (= 200 (:status response))
+      (json/parse-string (:body response) true)
+      (println "Erro ao obter tipos de odds:" (:status response)))))
+
+(defn obter-torneios [sport]
+  (let [url "https://betano.p.rapidapi.com/tournaments"
+        response (client/get url {:headers api-headers :query-params {:sport sport}})]
+    (if (= 200 (:status response))
+      (json/parse-string (:body response) true)
+      (println "Erro ao obter torneios:" (:status response)))))
+
+(defn obter-eventos [tournament-id]
+  (let [url "https://betano.p.rapidapi.com/events"
+        response (client/get url {:headers api-headers :query-params {:tournamentId tournament-id}})]
+    (if (= 200 (:status response))
+      (json/parse-string (:body response) true)
+      (println "Erro ao obter eventos:" (:status response)))))
+
+(defn obter-odds [event-id]
+  (let [url "https://betano.p.rapidapi.com/odds_betano"
+        response (client/get url {:headers api-headers :query-params {:eventId event-id
+                                                                      :oddsFormat "decimal"
+                                                                      :raw "false"}})]
+    (if (= 200 (:status response))
+      (json/parse-string (:body response) true)
+      (println "Erro ao obter odds:" (:status response)))))
